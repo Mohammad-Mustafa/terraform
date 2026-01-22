d27845e51fc055db4bcd71be6e1afc5f425f552f
Mohammad Mustafa <mohammadmustafa2k19@gmail.com>
2026-01-22 14:08:04 +0530

    Day 10 lifecycle and locals multi SG combo ignored. I will come again in final project

---
+++ Day10-tf-lifecycle/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "name" {
+    ami = "ami-085f043560da76e08"
+    instance_type = "t3.small"
+    subnet_id = "subnet-097b15f1752cc402d"
+  
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = [ tags, instance_type ]
+    create_before_destroy = true
+  }
+}
