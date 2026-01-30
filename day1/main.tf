cdf5e260ac5dfe8490481642fa9735952da359cc
Mohammad-Mustafa <52414461+Mohammad-Mustafa@users.noreply.github.com>
2026-01-31 00:11:21 +0530

    Update main.tf

--- day1/main.tf
+++ day1/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "namee"{
-    ami = "ami-085f043560da76e08"
+    ami = "ami-085f043560da76e098"
     instance_type = "t3.small"
     subnet_id = "subnet-097b15f1752cc402d"
   
